b916061790efeb6c0d9af5c9955aaf1f012ab698
Labeya <laurentmwn@gmail.com>
2026-05-12 06:00:09 +0100

    refactor(presentation): introduce SessionOutput DTO

--- src/IdentityAndAccess/Presentation/Output/SessionOutput.php
+++ src/IdentityAndAccess/Presentation/Output/SessionOutput.php
@@ -2,12 +2,20 @@
 
 namespace App\IdentityAndAccess\Presentation\Output;
 
+use Symfony\Component\Serializer\Attribute\SerializedName;
+
 final class SessionOutput
 {
    public function __construct(
       public string $id,
+
+      #[SerializedName('created_at')]
       public string $createdAt,
+
+      #[SerializedName('ip_address')]
       public ?string $ipAddress = null,
+
+      #[SerializedName('user_agent')]
       public ?string $userAgent = null,
    ) {}
 }
