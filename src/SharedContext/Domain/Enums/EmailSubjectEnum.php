835e642e7b93fd931598b756e852b68552c1e0b3
Labeya <laurentmwn@gmail.com>
2026-05-13 02:25:19 +0100

    chore(cleanup): remove deprecated files and organize templates
    - Remove unused OTP enums and value objects
    - Remove deprecated magic link generator
    - Reorganize message templates under templates/messages/

--- src/SharedContext/Domain/Enums/EmailSubjectEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\SharedContext\Domain\Enums;
-
-enum EmailSubjectEnum: string
-{
-   case MAGIC_LINK = 'Your magic login link';
-   case RESET_PASSWORD = 'Reset your password';
-}
--- src/SharedContext/Domain/Enums/EmailTemplateEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\SharedContext\Domain\Enums;
-
-enum EmailTemplateEnum: string
-{
-   case MAGIC_LINK = 'emails/auth/magic_link.html.twig';
-   case RESET_PASSWORD = 'emails/auth/reset_password.html.twig';
-}
