bfad4b8a318c986a46abd4059bc6a7ca0de204fd
Labeya <laurentmwn@gmail.com>
2026-05-15 21:13:51 +0100

    feat(auth): add recovery DTOs and validation

---
+++ src/IdentityAndAccess/Presentation/Input/IdentifierInput.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\IdentityAndAccess\Presentation\Input;
+
+use App\IdentityAndAccess\Presentation\Contraints\PhoneOrEmail;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class IdentifierInput
+{
+   #[Assert\NotBlank()]
+   #[PhoneOrEmail()]
+   private ?string $identifier = null;
+
+   public function getIdentifier(): ?string
+   {
+      return $this->identifier;
+   }
+
+   public function setIdentifier(?string $identifier): static
+   {
+      $this->identifier = $identifier;
+
+      return $this;
+   }
+}
